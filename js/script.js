7264580c1ea888e9aeca55945b48fa6fb20ccb68
Vitaly Bryukhovetsky <bruhovet@list.ru>
2018-06-30 13:24:50 +0300

    Добавлена возможность закрытия попапов по Esc

--- js/script.js
+++ js/script.js
@@ -20,10 +20,12 @@ var openFedbackPopup = function () {
   feedbackEl.classList.add('popup-visible');
   feedbackPreset();
   feedbackTriggerEl.removeEventListener('click', onFeedbackTriggerClick);
+  mapShowTrigger.removeEventListener('click', onMapTriggerClick);
   overlayEl.addEventListener('click', onOverlayClick);
   closeFeedbackEl.addEventListener('click', onClosefeedbackClick);
   feedbackFormEl.addEventListener('submit', onFormSubmit);
   feedbackFormEl.addEventListener('input', onInput);
+  document.addEventListener('keydown', onEscKeyDown);
 }
 
 var closeFedbackPopup = function () {
@@ -31,10 +33,23 @@ var closeFedbackPopup = function () {
   feedbackEl.classList.remove('popup-visible');
   inputElts.forEach(function(it) { it.classList.remove('feedback-input--invalid'); });
   feedbackTriggerEl.addEventListener('click', onFeedbackTriggerClick);
+  mapShowTrigger.addEventListener('click', onMapTriggerClick);
   overlayEl.removeEventListener('click', onOverlayClick);
   closeFeedbackEl.removeEventListener('click', onClosefeedbackClick);
   feedbackFormEl.removeEventListener('submit', onFormSubmit);
   feedbackFormEl.reset();
+  document.removeEventListener('keydown', onEscKeyDown);
+}
+
+var closePopup = function () {
+  feedbackEl.classList.contains('popup-visible') ? closeFedbackPopup()
+      : onCloseMapClick();
+}
+
+var onEscKeyDown = function (evt) {
+  if (evt.key === 'Escape') {
+    closePopup();
+  }
 }
 
 var feedbackPreset = function () {
@@ -108,14 +123,20 @@ var onMapTriggerClick = function () {
   overlayEl.classList.add('popup-visible');
   mapEl.classList.add('popup-visible');
   mapShowTrigger.removeEventListener('click', onMapTriggerClick);
+  feedbackTriggerEl.removeEventListener('click', onFeedbackTriggerClick);
+  overlayEl.addEventListener('click', onCloseMapClick);
   closeMapEl.addEventListener('click', onCloseMapClick);
+  document.addEventListener('keydown', onEscKeyDown);
 }
 
 var onCloseMapClick = function () {
   overlayEl.classList.remove('popup-visible');
   mapEl.classList.remove('popup-visible');
   mapShowTrigger.addEventListener('click', onMapTriggerClick);
+  feedbackTriggerEl.addEventListener('click', onFeedbackTriggerClick);
+  overlayEl.removeEventListener('click', onCloseMapClick);
   closeMapEl.removeEventListener('click', onCloseMapClick);
+  document.removeEventListener('keydown', onEscKeyDown);
 }
 
 feedbackTriggerEl.addEventListener('click', onFeedbackTriggerClick);
